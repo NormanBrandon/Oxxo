deeb44fd8c7f5641506761167bce271bbe00a3ad
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2018-04-26 10:31:58 -0500

    Pagina del oxxo

---
+++ js/app.js
@@ -0,0 +1,7 @@
+ $(document).ready(function(){
+    $('.parallax').parallax();
+     $('.slider').slider();
+      $('.materialboxed').materialbox();
+    $('.carousel').carousel();
+  });
+        
